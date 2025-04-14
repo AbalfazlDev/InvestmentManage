455949d128d8e9e91c5237385144800154222160
Abalfazl <abalfazldev@gmail.com>
2025-04-14 18:28:52 +0330

    feat : setting add settings items

---
+++ InvestmentManage.Presentation/ViewModels/Setting/MainSettingVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static InvestmentManage.Domain.Model.MarketCategory.MarketTypeM;
+
+namespace InvestmentManage.Presentation.ViewModels.Setting
+{
+    public class MainSettingVM
+    {
+        public MainSettingVM()
+        {
+            LVSettingItems = new ObservableCollection<SettingItems>(Enum.GetValues(typeof(SettingItems)).Cast<SettingItems>());
+        }
+        public ObservableCollection<SettingItems> LVSettingItems { get; set; }
+
+        public enum SettingItems
+        {
+            Font,
+            Theme,
+            Databases
+        }
+    }
+}
